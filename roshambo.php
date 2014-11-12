9abebefca5a578c0963d16535b9d703fa26af029
amyvs <amydecat@yahoo.com>
2014-11-12 12:17:45 -0800

    move winning stats to center of diagram

--- roshambo.php
+++ roshambo.php
@@ -24,33 +24,27 @@
 				<input type="submit">
 			</form>
 		</div>
-			<div class="col-xs-4">
-				<?php
-					if ((($player1 === "rock" || $player1 === "scissors") && $player2 === "lizard") ||
-						(($player1 === "lizard" || $player1 === "paper") && $player2 === "spock") ||
-						(($player1 === "spock" || $player1 === "rock") && $player2 === "scissors") ||
-						(($player1 === "scissors" || $player1 === "lizard") && $player2 === "paper") ||
-						(($player1 === "paper" || $player1 === "spock") && $player2 === "rock")) {
-						echo "<p class='winner'>Player One Wins!</p>";
-					} elseif ($player1 === $player2) {
-						echo "<p class='winner'>It's a tie!</p>";
-					} else {
-						echo "<p class='winner'>Player Two wins!</p>";
-					}
-				?>
-			</div>
-			<div class="col-xs-4">
-				<?php echo "<div id='player2'>Player Two: {$player2}</div>"?>
-			</div>
-		</div>
 	</div>
 	<div class="container diagram">
 		<div class="row">
 			<p class="col-xs-3 col-xs-offset-4 item" id="rock">rock</p>
 		</div>
 		<div class="row row-two">
-			<p class="col-xs-3 col-xs-offset-1 item" id="paper">paper</p>
-			<p class="col-xs-3 col-xs-offset-3 item" id="lizard">lizard</p>
+			<p class="col-xs-3 item" id="paper">paper</p>
+			<?php
+				if ((($player1 === "rock" || $player1 === "scissors") && $player2 === "lizard") ||
+					(($player1 === "lizard" || $player1 === "paper") && $player2 === "spock") ||
+					(($player1 === "spock" || $player1 === "rock") && $player2 === "scissors") ||
+					(($player1 === "scissors" || $player1 === "lizard") && $player2 === "paper") ||
+					(($player1 === "paper" || $player1 === "spock") && $player2 === "rock")) {
+					echo "<p class='col-xs-3 item' id='winning-stats'>{$player1} against {$player2}: player one wins!</p>";
+				} elseif ($player1 === $player2) {
+					echo "<p class='col-xs-3 item' id='winning-stats'>{$player1} against {$player2}: it's a tie!</p>";
+				} else {
+					echo "<p class='col-xs-3 item' id='winning-stats'>{$player1} against {$player2}: player two wins!</p>";
+				}
+			?>
+			<p class="col-xs-3 item" id="lizard">lizard</p>
 		</div>
 		<div class="row row-three">
 			<p class="col-xs-3 col-xs-offset-2 item" id="scissors">scissors</p>
